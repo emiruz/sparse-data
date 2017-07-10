d4a6e4beb0a90e7e0b0adf58b22d4cfb56023bae
emiruz <emir@usgroupltd.uk>
2017-07-10 09:49:47 +0100

    Changing nested defs to lets

--- src/sparse_data/core.clj
+++ src/sparse_data/core.clj
@@ -8,7 +8,7 @@
     (vec
      (mapcat (fn [[k v]]
                (let [sub (flatten-map v)
-                     nested (map #(into [k] %) (filter (comp not empty?) sub))]
+                     nested (map #(into [k] %) (filter seq sub))]
                  (if (seq nested)
                    nested
                    [[k v]])))
@@ -16,10 +16,10 @@
 
 (defn make-spec[coll]
   "Create a new column spec from a collection"
-  (defn f [x y]
-    (let [o (flatten-map y)]
-      (reduce #(update-in %1 [%2] (constantly false)) x o)))
-  (let [o (reduce f {} coll)]
+  (let [f (fn [x y]
+            (let [o (flatten-map y)]
+              (reduce #(update-in %1 [%2] (constantly false)) x o)))
+        o (reduce f {} coll)]
     (doall
      (zipmap
       (filter #(some? (last %))(keys o))
@@ -47,23 +47,23 @@
 
 (defn select [spec fname fields]
   "select a lazy seq of values from a sparse data file according to the fields specified."
-  (def m
-    (if (= fields :all)
-      spec
-      (filter some? (map (fn[x] (if (some #(= (butlast (first x)) %) fields) x)) spec))))
-  (def cols (into {} (map (fn[x][(last x) (first x)]) m)))
-  (letfn
-      [(helper [rdr]
-         (lazy-seq
-          (if-let [line (.readLine rdr)]
-            (cons
-             (into
-              {}
-              (keys
-               (select-keys
-                spec
-                (vals
-                 (select-keys cols (map #(Long/parseLong % 36) (str/split line #"\t")))))))
-                 (helper rdr))
-                (do (.close rdr) nil))))]
-    (helper (-> fname io/input-stream java.util.zip.GZIPInputStream. io/reader))))
+  (let
+      [m (if (= fields :all)
+           spec
+           (filter some? (map (fn[x] (if (some #(= (butlast (first x)) %) fields) x)) spec)))
+       cols (into {} (map (fn[x][(last x) (first x)]) m))]
+    (letfn
+        [(helper [rdr]
+           (lazy-seq
+            (if-let [line (.readLine rdr)]
+              (cons
+               (into
+                {}
+                (keys
+                 (select-keys
+                  spec
+                  (vals
+                   (select-keys cols (map #(Long/parseLong % 36) (str/split line #"\t")))))))
+               (helper rdr))
+              (do (.close rdr) nil))))]
+      (helper (-> fname io/input-stream java.util.zip.GZIPInputStream. io/reader)))))
